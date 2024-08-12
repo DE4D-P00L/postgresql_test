bfaa09be4cf13a26d25b6ec6267856ec724b2d96
Prashant <jatinverma200025@gmail.com>
2024-08-12 22:45:16 +0530

    Added db

---
+++ src/actions/actions.js
@@ -0,0 +1,20 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+export const getPosts = async () => {
+  const posts = await prisma.post.findMany({
+    where: {
+      published: true,
+    },
+    include: {
+      author: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  return posts;
+};
